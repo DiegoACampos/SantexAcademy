ad9e43792e739bf4c9f0b70a376ccec5edc67011
DiegoACampos <99999011+DiegoACampos@users.noreply.github.com>
2023-08-13 20:10:47 -0300

    Actualizar enrolled.model.js

--- back/models/enrolled.model.js
+++ back/models/enrolled.model.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Enrolled.belongsTo(models.User, {
+      models.User.belongsTo(Enrolled, {
         sourceKey: 'id',
       });
     }
